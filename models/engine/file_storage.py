621e0d7011600cb794f9593f0e4333caaa6c273b
Gustavo <gdponcemarsiglia@gmail.com>
2023-02-23 10:08:49 -0800

    tasks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,3 +36,6 @@ class FileStorage:
 
     def reload(self):
         """  deserializes the JSON file to __objects """
+        #deserializes the JSON file to __objects (only if the JSON file (__file_path) exists
+
+        #otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
