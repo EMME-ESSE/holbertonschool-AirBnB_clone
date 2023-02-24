93483b3d28faa8a4fda0312cac5bb32bdd65a7fc
EMME-ESSE <spiritexdi@gmail.com>
2023-02-24 14:44:25 -0300

    Updated base models

--- models/base_model.py
+++ models/base_model.py
@@ -38,4 +38,4 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = datetime.now()
