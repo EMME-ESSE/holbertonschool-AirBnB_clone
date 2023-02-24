9c9a69594a574650ff600aecddafdb43233b23bc
EMME-ESSE <spiritexdi@gmail.com>
2023-02-24 00:45:56 -0300

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -17,6 +18,7 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def to_dict(self):
         """Returns a dictionary containing all instance attributes"""
