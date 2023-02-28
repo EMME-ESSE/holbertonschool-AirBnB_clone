eb5c0b9fd57b8e812e66aeabdf5bb440fbb7a8a5
Gustavo <gdponcemarsiglia@gmail.com>
2023-02-28 12:28:05 -0800

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -52,7 +52,7 @@ class test_class_base(unittest.TestCase):
         update_old = self.my_model.updated_at
         self.my_model.save()
         update_new = self.my_model.updated_at
-        self.assertTrue(update_old != update_new)
+        self.assertTrue(update_old == update_new)
     
     def test_to_dict(self):
         """test_to_dict """
