b7f08e1dbe0a11682dc6decf69f60fdbefff1a10
EMME-ESSE <spiritexdi@gmail.com>
2023-02-28 21:04:54 -0300

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" storage test """
+""" test module file storage """
 
 import json
 import unittest
@@ -10,17 +10,9 @@ from models.base_model import BaseModel
 from models.engine import file_storage
 from models.engine.file_storage import FileStorage
 
-import models
-from models.user import User
-from models.state import State
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
 
 class test_file_storage(unittest.TestCase):
-    """ test class storage """
+    """ test class file storage """
 
     def setUp(self):
         """ set instance class """
@@ -41,35 +33,25 @@ class test_file_storage(unittest.TestCase):
             self.assertGreater(len(f.read()), 0)
 
     def test_reload(self):
-        """ test reload """
-        bm = BaseModel()
-        us = User()
-        st = State()
-        pl = Place()
-        cy = City()
-        am = Amenity()
-        rv = Review()
-        models.storage.new(bm)
-        models.storage.new(us)
-        models.storage.new(st)
-        models.storage.new(pl)
-        models.storage.new(cy)
-        models.storage.new(am)
-        models.storage.new(rv)
-        models.storage.save()
-        models.storage.reload()
-        objs = FileStorage._FileStorage__objects
-        self.assertIn("BaseModel." + bm.id, objs)
-        self.assertIn("User." + us.id, objs)
-        self.assertIn("State." + st.id, objs)
-        self.assertIn("Place." + pl.id, objs)
-        self.assertIn("City." + cy.id, objs)
-        self.assertIn("Amenity." + am.id, objs)
-        self.assertIn("Review." + rv.id, objs)
+        """ test reload from json """
+        self.my_model.name = "My_first_model"
+        self.my_model.my_number = 89
+        name = str(self.my_model.__class__.__name__)
+        key = name + "." + str(self.my_model.id)
+        self.my_model.save()
+        self.storage.reload()
+        objs = self.storage.all()
+        self.assertIsNotNone(objs[key])
+        self.obj_reload = objs[key]
+        self.assertTrue(self.my_model.__dict__ == self.obj_reload.__dict__)
+        self.assertTrue(self.my_model is not self.obj_reload)
+        self.assertIsInstance(self.obj_reload, BaseModel)
+        self.assertTrue(self.storage.all(), "My_first_model")
 
     def test_inst(self):
         """ test instance """
         i = FileStorage()
+        self.assertEqual(i.path(), "file.json")
         bm = BaseModel()
         i.new(bm)
         self.assertGreater(len(i.all()), 0)
